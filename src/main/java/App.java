d8db8096e7faa3e73d772f5ff2be808497139a21
Daren Schaad and Ryan King <ryanking157@hotmail.com>
2016-02-22 11:55:08 -0800

    start making a DB.

---
+++ src/main/java/App.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+
+    get ("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post ("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String categoryName = request.queryParams("categoryName");
+      Category newCategory = new Category(categoryName);
+      model.put("")
+    }, new VelocityTemplateEngine());
+
+  }
+}
